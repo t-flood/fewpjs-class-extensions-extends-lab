0dc3ea8f7f0f428453398fc8f877c4cc4545067d
Tim Flood <tflood9@gmail.com>
2021-03-09 11:56:55 -0500

    commit tree 2e7baf3bc0b32901fa80f158684246bdf9493d17

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+class Polygon {
+  constructor(array) {
+    this.array = array;
+    this.count = this.array.length;
+  }
+  get countSides() {
+    return this.array.legth;
+  }
+
+  get perimeter() {
+    const add = (accumulator, currentValue) => accumulator + currentValue;
+    return this.array.reduce(add, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (!Array.isArray(this.array)) return;
+    if (this.count != 3) return;
+    let side1 = this.array[0];
+    let side2 = this.array[1];
+    let side3 = this.array[2];
+    return side1 == side2 && side1 == side3 && side2 == side3;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (!Array.isArray(this.array)) return;
+    if (this.count != 4) return;
+    let side1 = this.array[0];
+    let side2 = this.array[1];
+    let side3 = this.array[2];
+    let side4 = this.array[3];
+    return side1 == side2 && side1 == side3 && side1 == side4;
+  }
+
+  get area() {
+    return this.array[0] * this.array[1];
+  }
+}
